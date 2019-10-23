07001b331fb47676de630332c54acd233876e8ee
chaitanya150895 <chaitanya150895@gmail.com>
2019-10-23 14:55:22 +0530

    removed the replaceunderscoredpipe from app.module.ts created sharedModule and imported the pipe in to declareation and export and imported that shared module into the import and used it into it's html and it worked

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReplaceUnderscorePipe } from '../replace-underscore.pipe';
+
+
+
+@NgModule({
+  declarations: [    ReplaceUnderscorePipe
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [    ReplaceUnderscorePipe
+  ]
+})
+
+export class SharedModule { }
